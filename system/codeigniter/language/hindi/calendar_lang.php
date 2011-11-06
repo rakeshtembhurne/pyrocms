bf2de209d3f10632f76c908bb054b4aaf6064d0a
Rakesh Tembhurne <rakesh@tembhurne.com>
2011-11-06 19:13:39 +0530

    Hindi language files for codeigniter

---
+++ system/codeigniter/language/hindi/calendar_lang.php
@@ -0,0 +1,51 @@
+<?php
+
+$lang['cal_su']			= "र";
+$lang['cal_mo']			= "सो";
+$lang['cal_tu']			= "मं";
+$lang['cal_we']			= "बु";
+$lang['cal_th']			= "गु";
+$lang['cal_fr']			= "शु";
+$lang['cal_sa']			= "श";
+$lang['cal_sun']		= "र";
+$lang['cal_mon']		= "सोम";
+$lang['cal_tue']		= "मंगल";
+$lang['cal_wed']		= "बुध";
+$lang['cal_thu']		= "गुरु";
+$lang['cal_fri']		= "शुक्र";
+$lang['cal_sat']		= "शनि";
+$lang['cal_sunday']		= "रविवार";
+$lang['cal_monday']		= "सोमवार";
+$lang['cal_tuesday']	= "मंगलवार";
+$lang['cal_wednesday']	= "बुधवार";
+$lang['cal_thursday']	= "गुरुवार";
+$lang['cal_friday']		= "शुक्रवार";
+$lang['cal_saturday']	= "शनिवार";
+$lang['cal_jan']		= "जन";
+$lang['cal_feb']		= "फर";
+$lang['cal_mar']		= "मार्च";
+$lang['cal_apr']		= "अप्रे";
+$lang['cal_may']		= "मई";
+$lang['cal_jun']		= "जून";
+$lang['cal_jul']		= "जुला";
+$lang['cal_aug']		= "अग";
+$lang['cal_sep']		= "सित";
+$lang['cal_oct']		= "अक्टू";
+$lang['cal_nov']		= "नव";
+$lang['cal_dec']		= "दिस";
+$lang['cal_january']	= "जनवरी";
+$lang['cal_february']	= "फरवरी";
+$lang['cal_march']		= "मार्च";
+$lang['cal_april']		= "अप्रेल";
+$lang['cal_mayl']		= "मई";
+$lang['cal_june']		= "जून";
+$lang['cal_july']		= "जुलाई";
+$lang['cal_august']		= "अगस्त";
+$lang['cal_september']	= "सितम्बर";
+$lang['cal_october']	= "अक्टूबर";
+$lang['cal_november']	= "नवम्बर";
+$lang['cal_december']	= "दिसम्बर";
+
+
+/* End of file calendar_lang.php */
+/* Location: ./system/language/hindi/calendar_lang.php */
